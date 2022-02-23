03507d03d340d2aa59700555c25c729f3f66b19c
Jose Angel Rodriguez <joseangelrodriguez@STF-JR-303.local>
2022-02-23 13:35:47 -0600

    Se configuran los archivos de la API, .env, se manda a llamar la API desde el Frontend, se instala axios en el proyecto.

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getPokemons } from '../../api/getPokemons';
 import PokemomList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
 function Home() {
+  useEffect(() => {
+    getPokemons().then(res => console.log(res));
+  });
+
   return (
     <div className='Home'>
       <Searcher />
